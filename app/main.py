77de9f4c7046e72b5e5c1932a8c44157d71c78a0
KD-TrendOn <damirkrasilnicov2006@gmail.com>
2024-09-19 00:06:35 +0300

    вот

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import submission, tasks, reports
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"], # Allows all methods
+    allow_headers=["*"], # Allows all headers
+    )
 
 app.include_router(submission.router, prefix="/api/v1")
 app.include_router(tasks.router, prefix="/api/v1")
