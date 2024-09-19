c787b0c68e7f0d6d35857644da31a4b119042fee
KD-TrendOn <damirkrasilnicov2006@gmail.com>
2024-09-19 03:45:47 +0300

    все ближе к деплою

--- app/api/v1/endpoints/reports.py
+++ app/api/v1/endpoints/reports.py
@@ -2,18 +2,26 @@
 from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.schemas.reports import ReportResponse
+from app.schemas.reports import ParkReport
 from db.crud import get_submissions_within_park
 from db.db_helper import db_helper
+from collections import Counter
 
 router = APIRouter(
     prefix="/reports",
     tags=["Reports"],
 )
 
-@router.get("/park", response_model=List[ReportResponse])
+@router.get("/park", response_model=ParkReport)
 async def get_park_report(
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     submissions = await get_submissions_within_park(session)
-    return [ReportResponse.from_orm(sub) for sub in submissions]
+    
+    # Подсчет количества объектов каждого класса
+    class_counts = Counter(sub.object_class for sub in submissions)
+    
+    # Создание словаря с количеством для каждого класса
+    statistics = {class_name: class_counts.get(class_name, 0) for class_name in CLASSES}
+    
+    return ParkReport(statistics=statistics)
--- app/schemas/reports.py
+++ app/schemas/reports.py
@@ -1,13 +1,5 @@
-# app/schemas/reports.py
+from pydantic import BaseModel
+from typing import Dict
 
-from pydantic import BaseModel, ConfigDict
-from datetime import datetime
-
-class ReportResponse(BaseModel):
-    id: int
-    object_class: str
-    latitude: float
-    longitude: float
-    created_at: datetime
-
-    model_config = ConfigDict(from_attributes=True)
+class ParkReport(BaseModel):
+    statistics: Dict[str, int]
